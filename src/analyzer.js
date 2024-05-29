dd3ee60cbae58067ee18a97e94314cb28c93ae3c
daniela trejo <danielatrejogo19@gmail.com>
2024-05-29 00:52:11 -0600

    cambios de metricas de analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,29 +1,47 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {  
     const words = text.trim ().split(/\s+/).filter(word => word.length >0);
-    return words.lenght;
-    } 
+    const wordCount = words.length;
+    return (wordCount);
+
+    }, 
   //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  }
+  
   getcharactercount:(text) =>{
-   return text.length; 
-  }
+    const characterCount = text.length;
+   return (characterCount);
+  },
       //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
 
   getCharacterCountExcludingSpac : (text) => {
     const characters = text.replace(/\s+/g, '');
-    return characters.length;
+    const characterEs = characters.length;
+    return (characterEs);
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-  }
-  getAverageWordLength: (text) => {    
+  },
+  getAverageWordLength: (text) => {   
+    const words = text.trim().split(/\s+/).filter(word => word.length > 0);
+    if (words.lenght === 0);
+    const totalLength = words.reduce((sum, word)=> sum+ word.length, 0);
+    const averageWord =(totalLength / words.length).toFixed(2); 
+    return (averageWord);
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
+    const numbers = text.match(/\d+/g) || [];
+    const numberCount =numbers.length;
+    return (numberCount);
+
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
+    const numbers = text.match(/\d+/g) ||[];
+    const numberSum =numbers.reduce((sum,num) => sum + parseInt(num, 10), 0);
+    return (numberSum);
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
+  
+  };
 
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 import analyzer from './analyzer.js';
 
+const boton = document.getElementById("reset-button");
+const palabras = document.querySelector("li.list-item:nth-child(1)");
+const caracteres = document.querySelector("li.list-item:nth-child(2)");
+const caracteresSe = document.querySelector("li.list-item:nth-child(3)");
+const numeros = document.querySelector("li.list-item:nth-child(4)" );
+const suma = document.querySelector("li.list-item:nth-child(5)");
+const longitud =document.querySelector("li.list-item:nth-child(6)");
+const cajaTexto =document.querySelector(`textarea`);
+
+//metricas
+cajaTexto.addEventListener("input",metricas)
+function metricas(){
+    const textoFinal= cajaTexto.value;
+    
+    //contabilizar palabras
+   const wordCount = analyzer.getWordCount(textoFinal);
+   palabras.innerHTML="Recuento de palabras :" + wordCount;
+
+   //contabilizar caracteres 
+   const characterCount  = analyzer.getcharactercount_(textoFinal);
+   caracteres.innerHTML="Recuento de caracteres :"+ characterCount ;
+
+   //contabilizar sin espacios y sin nunmero 
+   const characterEs  = analyzer.getCharacterCountExcludingSpac(textoFinal);
+   caracteresSe.innerHTML="Recuento de caracteres sin espacio y signos:" + characterEs ;
+
+   //contabilizar numeros
+   const numberCount =analyzer.getNumberCount(textoFinal);
+   numeros.innerHTML="Recuento de numeros:" +numberCount;
+
+   //Suma denumeros
+   const numberSum= analyzer.getNumberSum(textoFinal);
+   suma.innerHTML="Suma total de numeros:" + numberSum;
+
+   //longuitud del texto
+   const averageWord= analyzer.getAverageWordLength(textoFinal);
+   longitud.innerHTML="Longitud media depalabras:"+averageWord;
+
+}
+
+boton.addEventListener("click",function(){
+    if(cajaTexto.value===''){
+    alert("El cuadro esta listo para que escribas en el");
+    }else{
+        cajaTexto.vale='';
+    }
+    palabras.innerHTML ="Recuento de palabras:0"
+    caracteres.innerHTML ="Recuento de caracteres:0"
+    caracteresSe.innerHTML ="Recuento de caracteres sin espacios y signos:0"
+    numeros.innerHTML="Recuento de numeros :0"
+    suma.innerHTML="Suma total de numeros :0"
+    longitud.innerHTML="Longitud media de palabras :0"
+    })
+
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
