94b44163d177582c3cbce20be379ec5b5f901dde
daniela trejo <danielatrejogo19@gmail.com>
2024-06-01 00:24:29 -0600

    ultimo cambios y pruebas

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,47 +1,51 @@
 const analyzer = {
-  getWordCount: (text) => {  
-    const words = text.trim ().split(/\s+/).filter(word => word.length >0);
-    const wordCount = words.length;
-    return (wordCount);
-
-    }, 
-  //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  
-  getcharactercount:(text) =>{
-    const characterCount = text.length;
-   return (characterCount);
+  //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro text de tipo string.
+  getWordCount: (textoFinal) => {
+    const words = textoFinal
+      .trim()
+      .split(/\s+/)
+      .filter((word) => word.length > 0);
+    const wordsCount = words.length;
+    return wordsCount;
   },
-      //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-
-  getCharacterCountExcludingSpac : (text) => {
-    const characters = text.replace(/\s+/g, '');
-    const characterEs = characters.length;
-    return (characterEs);
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+  //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro text de tipo string.
+  getCharacterCount: (textoFinal) => {
+    const ccaracteres = textoFinal;
+    const caracteresFinales = ccaracteres.length;
+    return caracteresFinales;
   },
-  getAverageWordLength: (text) => {   
-    const words = text.trim().split(/\s+/).filter(word => word.length > 0);
-    if (words.lenght === 0);
-    const totalLength = words.reduce((sum, word)=> sum+ word.length, 0);
-    const averageWord =(totalLength / words.length).toFixed(2); 
-    return (averageWord);
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro text de tipo string.
+  getCharacterCountExcludingSpaces: (textoFinal) => {
+    const caracteresSEN = textoFinal.replace(/\W/g, "");
+    const caracteresSENR = caracteresSEN.length;
+    return caracteresSENR;
   },
-  getNumberCount: (text) => {
-    const numbers = text.match(/\d+/g) || [];
-    const numberCount =numbers.length;
-    return (numberCount);
-
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  //TODO: esta función debe retornar cúantos números se encuentran en el parámetro text de tipo string.
+  getNumberCount: (textoFinal) => {
+    const numberosc = textoFinal.match(/\d+(\.\d+)?(?!\w)/g) || [];
+    const numerosR = numberosc.length;
+    return numerosR;
   },
-  getNumberSum: (text) => {
-    const numbers = text.match(/\d+/g) ||[];
-    const numberSum =numbers.reduce((sum,num) => sum + parseInt(num, 10), 0);
-    return (numberSum);
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+  //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro text de tipo string.
+  getNumberSum: (textoFinal) => {
+    const sumaNumeros = textoFinal.match(/\d+(\.\d+)?(?!\w)/g);
+    let sumaNumerosR = 0;
+    if (sumaNumeros) {
+      for (let i = 0; i < sumaNumeros.length; i++) {
+        sumaNumerosR = sumaNumerosR + parseFloat(sumaNumeros[i]);
+      }
+    }
+    const sumaFinal = Number(sumaNumerosR);
+    return sumaFinal;
   },
-  
-  };
-
-
+  //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro text de tipo string.
+  getAverageWordLength: (textoFinal) => {
+    const longitudP = textoFinal.split(" ");
+    let longitudR = 0;
+    for (let i = 0; i < longitudP.length; i++) {
+      longitudR = longitudR + longitudP[i].length;
+    }
+    return parseFloat((longitudR / longitudP.length).toFixed(2));
+  },
+};
 export default analyzer;
