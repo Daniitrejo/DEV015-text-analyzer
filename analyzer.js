f181e2a33bfa584d695f46f44938cd9f299dfda5
daniela trejo <danielatrejogo19@gmail.com>
2024-06-05 20:58:15 -0600

    Updates

---
+++ analyzer.js
@@ -0,0 +1,51 @@
+const analyzer = {
+  //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro text de tipo string.
+  getWordCount: (textoFinal) => {
+    const words = textoFinal
+      .trim()
+      .split(/\s+/)
+      .filter((word) => word.length > 0);
+    const wordsCount = words.length;
+    return wordsCount;
+  },
+  //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro text de tipo string.
+  getCharacterCount: (textoFinal) => {
+    const ccaracteres = textoFinal;
+    const caracteresFinales = ccaracteres.length;
+    return caracteresFinales;
+  },
+  //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro text de tipo string.
+  getCharacterCountExcludingSpaces: (textoFinal) => {
+    const caracteresSEN = textoFinal.replace(/\W/g, "");
+    const caracteresSENR = caracteresSEN.length;
+    return caracteresSENR;
+  },
+  //TODO: esta función debe retornar cúantos números se encuentran en el parámetro text de tipo string.
+  getNumberCount: (textoFinal) => {
+    const numberosc = textoFinal.match(/\d+(\.\d+)?(?!\w)/g) || [];
+    const numerosR = numberosc.length;
+    return numerosR;
+  },
+  //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro text de tipo string.
+  getNumberSum: (textoFinal) => {
+    const sumaNumeros = textoFinal.match(/\d+(\.\d+)?(?!\w)/g);
+    let sumaNumerosR = 0;
+    if (sumaNumeros) {
+      for (let i = 0; i < sumaNumeros.length; i++) {
+        sumaNumerosR = sumaNumerosR + parseFloat(sumaNumeros[i]);
+      }
+    }
+    const sumaFinal = Number(sumaNumerosR);
+    return sumaFinal;
+  },
+  //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro text de tipo string.
+  getAverageWordLength: (textoFinal) => {
+    const longitudP = textoFinal.split(" ");
+    let longitudR = 0;
+    for (let i = 0; i < longitudP.length; i++) {
+      longitudR = longitudR + longitudP[i].length;
+    }
+    return parseFloat((longitudR / longitudP.length).toFixed(2));
+  },
+};
+export default analyzer;
